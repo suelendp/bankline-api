24c6cbff5c1500bb0f6fd43f1a9f28ffce31afab
Suélen Dias Palhares <suelenddf@gmail.com>
2022-05-05 19:39:50 -0300

    Add app

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -8,6 +8,7 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		//Começamos a codificar o nosso projeto Bankline - API
 	}
 
 }
